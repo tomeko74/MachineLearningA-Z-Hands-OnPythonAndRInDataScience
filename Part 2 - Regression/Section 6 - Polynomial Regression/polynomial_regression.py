6ec6d2d6ecd4e7761aaa3c0f6edf425ad14f90bb
tomeko74 <thomasz.orlowski@gmail.com>
2018-12-20 21:14:52 +0100

    finish

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -7,10 +7,11 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
+X = dataset.iloc[:, 1:2].values  # takie cięcie zwraca macierz jednokolumnową a nie wektor
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
+# mamy za mały zbiór danych, żeby go dzielić na uczący i testowy
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
@@ -60,7 +61,7 @@ plt.ylabel('Salary')
 plt.show()
 
 # Predicting a new result with Linear Regression
-lin_reg.predict(6.5)
+print(lin_reg.predict(np.array(6.5).reshape(1,-1)))
 
 # Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
+print(lin_reg_2.predict(poly_reg.fit_transform(np.array(6.5).reshape(1,-1))))
