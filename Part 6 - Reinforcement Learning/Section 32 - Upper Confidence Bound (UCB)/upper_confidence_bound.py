3f12485f66ac11cd0baa049d023c602cab71198a
tomeko74 <thomasz.orlowski@gmail.com>
2019-01-19 14:36:07 +0100

    finish

--- Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -20,7 +20,7 @@ for n in range(0, N):
     ad = 0
     max_upper_bound = 0
     for i in range(0, d):
-        if (numbers_of_selections[i] > 0):
+        if numbers_of_selections[i] > 0:
             average_reward = sums_of_rewards[i] / numbers_of_selections[i]
             delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
             upper_bound = average_reward + delta_i
