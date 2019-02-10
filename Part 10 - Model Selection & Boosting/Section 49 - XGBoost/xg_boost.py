6a47be0eff7795f9ad44c9ef5a1cfd50e6315097
tomeko74 <thomasz.orlowski@gmail.com>
2019-02-10 15:06:59 +0100

    small fixes and name of file change

--- Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xg_boost.py
+++ Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xg_boost.py
@@ -18,13 +18,13 @@ labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
+onehotencoder = OneHotEncoder(categorical_features=[1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Fitting XGBoost to the Training set
 from xgboost import XGBClassifier
@@ -40,6 +40,6 @@ cm = confusion_matrix(y_test, y_pred)
 
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
 accuracies.mean()
 accuracies.std()
